2161ee731a9d4fafc841591768e23fcdf1dab87c
kevin-adb <kevin-adb@hotmail.com>
2024-08-12 17:37:37 +0000

    Deploy (2024-08-12T17:37:37Z)

---
+++ docs/scripts/about.467d400c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgk7_web=self.webpackChunkgk7_web||[]).push([[594],{5359:(l,e,r)=>{r.r(e),r.d(e,{AboutPage:()=>a});var t=r(7484),u=r(4015),i=r(6540),n=r(5168),a=(0,u.PA)((function(){var l=(0,n.RT)().B.U,e=(0,n.RW)().ee;return i.createElement("div",null,i.createElement("div",null,l,i.createElement(t.N,{href:"mailto:".concat(e)},e)))}))}}]);
