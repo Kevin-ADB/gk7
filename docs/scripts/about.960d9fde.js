362b8e254948463ffdd57e05ff436353aa0ffb1c
kevin-adb <kevin-adb@hotmail.com>
2024-08-17 07:31:35 +0000

    Deploy (2024-08-17T07:31:35Z)

---
+++ docs/scripts/about.960d9fde.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgk7_web=self.webpackChunkgk7_web||[]).push([[594],{5359:(l,e,r)=>{r.r(e),r.d(e,{AboutPage:()=>f});var t=r(7484),u=r(4015),i=r(6540),n=r(4845),a=r(4529),f=(0,u.PA)((function(){var l=(0,n.RT)().U.G,e=(0,a.R)().X;return i.createElement("div",null,i.createElement("div",null,l,i.createElement(t.N,{href:"mailto:".concat(e)},e)))}))}}]);
